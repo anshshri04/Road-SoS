28e8ab7e6ccae96b843e1af962469d5e2bf56284
Ansh Khare <ansh_xploit@192.168.1.119>
2026-05-22 19:52:18 +0530

    removed local ip fetch wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
+// import {StrictMode} from 'react';
+// import {createRoot} from 'react-dom/client';
+// import App from './App.tsx';
+// import './index.css';
+
+// const originalFetch = window.fetch;
+// window.fetch = async (input, init) => {
+//   if (typeof input === 'string' && input.startsWith('/api')) {
+//     input = 'http://192.168.1.119:3000' + input;
+//   }
+//   return originalFetch(input, init);
+// };
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// );
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
-const originalFetch = window.fetch;
-window.fetch = async (input, init) => {
-  if (typeof input === 'string' && input.startsWith('/api')) {
-    input = 'http://192.168.1.119:3000' + input;
-  }
-  return originalFetch(input, init);
-};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
